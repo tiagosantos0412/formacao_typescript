f28fa1a64fd1791438a8dd9000bcfaabaf56966b
Tiago <tiagosantos0412@gmail.com>
2024-01-29 05:55:35 -0300

    Estrutura de repetição com for e while.

---
+++ 1 - Introdução/1.5 - Estruturas de Repetição/app.ts
@@ -0,0 +1,15 @@
+// Estruturas de repetição
+
+// For:
+console.log('Laço de repetição com For')
+for(let i = 0; i < 5; i++){
+    console.log(i)
+}
+
+// While:
+console.log('Laço de repetição com While')
+let contador = 0
+while(contador < 10){
+    console.log(contador)
+    contador ++
+}
